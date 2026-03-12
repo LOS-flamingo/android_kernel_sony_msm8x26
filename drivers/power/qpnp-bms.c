71c5f3cdda616d2be4030062a9ce2301e5e442f1
infus38 <infus38@gmail.com>
2026-03-12 23:15:33 +0000

    power: qpnp-bms: add flamingo capacity smoothing
    Carry over from Sony kernel: implement capacity smoothing logic for
    flamingo to reduce SOC jitter at normal and low charge levels,
    preventing unstable charge readings and unexpected power behavior.

--- drivers/power/qpnp-bms.c
+++ drivers/power/qpnp-bms.c
@@ -99,6 +99,12 @@
 
 #define QPNP_BMS_DEV_NAME "qcom,qpnp-bms"
 
+#ifdef CONFIG_SONY_FLAMINGO
+#define CAPACITY_SMOOTH_TIME	60
+#define LOW_CAPACITY	5
+#define LOW_CAPACITY_SMOOTH_TIME	30
+#endif
+
 enum {
 	SHDW_CC,
 	CC
@@ -293,6 +299,9 @@ struct qpnp_bms_chip {
 	struct qpnp_vadc_chip		*vadc_dev;
 	struct qpnp_iadc_chip		*iadc_dev;
 	struct qpnp_adc_tm_chip		*adc_tm_dev;
+#ifdef CONFIG_SONY_FLAMINGO
+	bool			last_is_charging;
+#endif
 };
 
 static struct of_device_id qpnp_bms_match_table[] = {
@@ -912,6 +921,26 @@ static bool is_batfet_closed(struct qpnp_bms_chip *chip)
 	pr_debug("battery power supply is not registered\n");
 	return true;
 }
+
+#ifdef CONFIG_SONY_FLAMINGO
+static bool is_battery_maintain(struct qpnp_bms_chip *chip)
+{
+	union power_supply_propval ret = {0,};
+
+	if (chip->batt_psy == NULL)
+		chip->batt_psy = power_supply_get_by_name("battery");
+	if (chip->batt_psy) {
+		/* if battery has been registered, use the online property */
+		chip->batt_psy->get_property(chip->batt_psy,
+					POWER_SUPPLY_PROP_IS_MAINTAIN, &ret);
+		return !!ret.intval;
+	}
+
+	/* Default to true if the battery power supply is not registered. */
+	pr_debug("battery power supply is not registered\n");
+	return false;
+}
+#endif
 
 static int get_simultaneous_batt_v_and_i(struct qpnp_bms_chip *chip,
 					int *ibat_ua, int *vbat_uv)
@@ -1816,6 +1845,9 @@ static int report_voltage_based_soc(struct qpnp_bms_chip *chip)
 #define MAX_CATCHUP_SOC	(SOC_CATCHUP_SEC_MAX / SOC_CATCHUP_SEC_PER_PERCENT)
 #define SOC_CHANGE_PER_SEC		5
 #define REPORT_SOC_WAIT_MS		10000
+#ifdef CONFIG_SONY_FLAMINGO
+static bool first_boot = false;
+#endif
 static int report_cc_based_soc(struct qpnp_bms_chip *chip)
 {
 	int soc, soc_change;
@@ -1826,6 +1858,9 @@ static int report_cc_based_soc(struct qpnp_bms_chip *chip)
 	int batt_temp;
 	int rc;
 	bool charging, charging_since_last_report;
+#ifdef CONFIG_SONY_FLAMINGO
+	int pc, vbatt;
+#endif
 
 	rc = wait_event_interruptible_timeout(chip->bms_wait_queue,
 			chip->calculated_soc != -EINVAL,
@@ -1853,12 +1888,49 @@ static int report_cc_based_soc(struct qpnp_bms_chip *chip)
 	mutex_lock(&chip->last_soc_mutex);
 	soc = chip->calculated_soc;
 
+#ifdef CONFIG_SONY_FLAMINGO
+	get_battery_voltage(chip, &vbatt);
+	pc = interpolate_pc(chip->pc_temp_ocv_lut, 25, vbatt / 1000);
+	if (first_boot) {
+		soc = pc;
+		first_boot = false;
+		pr_debug("based on voltage boot up soc = %d\n", soc);
+	}
+#endif
+
 	last_change_sec = chip->last_soc_change_sec;
 	calculate_delta_time(&last_change_sec, &time_since_last_change_sec);
 
 	charging = chip->battery_status == POWER_SUPPLY_STATUS_CHARGING;
 	charging_since_last_report = charging || (chip->last_soc_unbound
 			&& chip->was_charging_at_sleep);
+#ifdef CONFIG_SONY_FLAMINGO
+	if (is_battery_maintain(chip)) {
+		soc = 100;
+		get_current_time(&last_change_sec);
+		calculate_delta_time(&last_change_sec, &time_since_last_change_sec);
+		chip->last_soc_change_sec = last_change_sec;
+	}
+	else {
+		if (!charging) {
+			pr_debug("pc = %d, soc = %d\n", pc, soc);
+			if ( pc > soc) {
+				soc = pc;
+			}
+		}
+		
+		if (!charging && chip->last_is_charging) {
+			soc = chip->last_soc;
+		}
+	}
+	
+	if (charging) {
+		chip->last_is_charging = true;
+	}
+	else {
+		chip->last_is_charging = false;
+	}
+#endif
 	/*
 	 * account for charge time - limit it to SOC_CATCHUP_SEC to
 	 * avoid overflows when charging continues for extended periods
@@ -1923,8 +1995,26 @@ static int report_cc_based_soc(struct qpnp_bms_chip *chip)
 
 		if (soc < chip->last_soc && soc != 0)
 			soc = chip->last_soc - soc_change;
+#ifdef CONFIG_SONY_FLAMINGO
+		if (soc > chip->last_soc)
+			soc = chip->last_soc + soc_change;
+#else			
 		if (soc > chip->last_soc && soc != 100)
 			soc = chip->last_soc + soc_change;
+#endif
+
+#ifdef CONFIG_SONY_FLAMINGO
+		if ( soc > LOW_CAPACITY) {
+			if (time_since_last_change_sec <= CAPACITY_SMOOTH_TIME) {
+				soc = chip->last_soc;
+			}
+		}
+		else {
+			if (time_since_last_change_sec <= LOW_CAPACITY_SMOOTH_TIME) {
+				soc = chip->last_soc;
+			}
+		}
+#endif
 	}
 
 	if (chip->last_soc != soc && !chip->last_soc_unbound)
@@ -1933,6 +2023,13 @@ static int report_cc_based_soc(struct qpnp_bms_chip *chip)
 	pr_debug("last_soc = %d, calculated_soc = %d, soc = %d, time since last change = %d\n",
 			chip->last_soc, chip->calculated_soc,
 			soc, time_since_last_change_sec);
+#ifdef CONFIG_SONY_FLAMINGO
+	if ((chip->last_soc != -EINVAL) && (soc != chip->last_soc) && (time_since_last_change_sec > CAPACITY_SMOOTH_TIME)) {
+		if (chip->bms_psy_registered) {
+			power_supply_changed(&chip->bms_psy);
+		}
+	}
+#endif
 	chip->last_soc = bound_soc(soc);
 	backup_soc_and_iavg(chip, batt_temp, chip->last_soc);
 	pr_debug("Reported SOC = %d\n", chip->last_soc);
